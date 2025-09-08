5f81c559d25de2758e7798fa7c0ae69c1eb24377
aditiadri <2105091@kiit.ac.in>
2025-09-09 00:22:57 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,39 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useDispatch } from 'react-redux'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useDispatch } from "react-redux";
 
-import {login} from "./store/authslice"
-import {logout} from "./store/authslice"
-import Footer  from './components/Footer/Footer'
-import authService from './appwrite/auth'
-import Header from './components/Header/Header'
+import { login } from "./store/authslice";
+import { logout } from "./store/authslice";
+import Footer from "./components/Footer/Footer";
+import authService from "./appwrite/auth";
+import Header from "./components/Header/Header";
 function App() {
-const[loading , setLoading]=useState(true)
-const dispatch=useDispatch()
-
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    authService.getCurrentUser()
-    .then((userData) => {
-      if (userData) {
-        dispatch(login({userData}))
-      } else {
-        dispatch(logout())
-      }
-    })
-    .finally(() => setLoading(false))
-  }, [])
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
   return !loading ? (
-   <div className='min-h-screen flex flex-wrap content-between bg-amber-200'>
-    <div className='w-full'>
-      <Header/>
-      <Footer/>
-      
+    <div className="min-h-screen flex flex-wrap content-between bg-amber-200">
+      <div className="w-full">
+        <Header />
+        <Footer />
+      </div>
     </div>
-   </div>
-  ):null
+  ) : null;
 }
 
-export default App
+export default App;
--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -2,55 +2,68 @@
 import { Client, Account, ID } from "appwrite";
 import conf from "../conf/conf"; // adjust path if needed
 
-
 export class AuthService {
-    client = new Client();
-    account;
+  client = new Client();
+  //account;
 
-    constructor() {
-        this.client
-            .setEndpoint(conf.appwriteUrl)
-            .setProject(conf.appwriteProjectId);
-        this.account = new Account(this.client);
-    }
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+    console.log(this.account);
+    console.log(this.client);
+  }
 
-    async createAccount({ email, password, name }) {
-        try {
-            const user = await this.account.create(ID.unique(), email, password, name);
-            if (user) {
-                return this.login({ email, password });
-            }
-            return user;
-        } catch (error) {
-            throw error;
-        }
+  async createAccount({ email, password, name }) {
+    try {
+      const user = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (user) {
+        return this.login({ email, password });
+      }
+      return user;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async login({ email, password }) {
-        try {
-            return await this.account.createEmailPasswordSession(email, password);
-        } catch (error) {
-            throw error;
-        }
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async getCurrentUser() {
-        try {
-            return await this.account.get();
-        } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error);
-            return null;
-        }
+  async getCurrentUser() {
+    try {
+        await this.account.createEmailPasswordSession({
+        email: "user@example.com",
+        password: "password",
+      });
+      return await this.account.get().
+      then((result) => {console.log(result);
+        return(result)
+      });
+      
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser :: error", error);
+      return null;
     }
+  }
 
-    // ✅ separate function for logout with try-catch
-    async logout() {
-        try {
-            await this.account.deleteSession("current");
-        } catch (error) {
-            console.log("Appwrite service :: logout :: error", error);
-        }
+  async logout() {
+    try {
+      await this.account.deleteSession("current");
+    } catch (error) {
+      console.log("Appwrite service :: logout :: error", error);
     }
+  }
 }
 
 const authService = new AuthService();
